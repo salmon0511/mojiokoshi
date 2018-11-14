2100f6691f3cf3e6ef7d69ebfe771073852193b9
Sakamoto Yuya <yuya@sakamotonoMacBook-Air.local>
2018-11-14 19:11:27 +0900

    secound commit

--- settings.py
+++ settings.py
@@ -11,3 +11,6 @@ load_dotenv(dotenv_path)
 # LINE
 YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
+
+#azure
+KEY1 = os.environ.get("KEY1")
